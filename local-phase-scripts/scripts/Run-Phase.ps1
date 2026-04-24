0d92af552b020346d48e3ce0fe2bfcdca4a342b0
MaarksN <130610135+MaarksN@users.noreply.github.com>
2026-04-24 13:14:27 -0300

    Update Run-Phase.ps1

--- local-phase-scripts/scripts/Run-Phase.ps1
+++ local-phase-scripts/scripts/Run-Phase.ps1
@@ -1,4 +1,4 @@
-param(
+﻿param(
   [Parameter(Mandatory=$true)]
   [ValidateSet("0","1","2","3","4","5","6","7","8","9","10","11","12","00","01","02","03","04","05","06","07","08","09")]
   [string]$Phase,
@@ -24,5 +24,10 @@ $map = @{
 }
 
 $script = Join-Path $PSScriptRoot ("phases\" + $map[$Phase])
-& powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File $script -RepoRoot $RepoRoot -ReportRoot $ReportRoot
+if ([string]::IsNullOrWhiteSpace($ReportRoot)) {
+  & powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File $script -RepoRoot $RepoRoot
+} else {
+  & powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File $script -RepoRoot $RepoRoot -ReportRoot $ReportRoot
+}
 exit $LASTEXITCODE
+
